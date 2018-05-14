38df8357e54bad1040dba53025e53792cc67f049
Jasonnor <wujason810@gmail.com>
2018-05-14 18:57:07 +0800

    Fix tap time

--- src/utility.lua
+++ src/utility.lua
@@ -1,6 +1,6 @@
 function ramdomSleep(n)
 	math.randomseed(os.time())
-    mSleep(n + math.random(-500, 500))
+	mSleep(n + math.random(-500, 500))
 end
 
 function ramdomCoordinate(n)
@@ -11,9 +11,9 @@ end
 function tap(x, y)
 	x = ramdomCoordinate(x)
 	y = ramdomCoordinate(y)
-    touchDown(1, x, y)
-    ramdomSleep(100)
-    touchUp(1, x, y)
+	touchDown(1, x, y)
+	ramdomSleep(50)
+	touchUp(1, x, y)
 end
 
 function drag(x1, y1, x2, y2)
@@ -22,8 +22,8 @@ function drag(x1, y1, x2, y2)
 	x2 = ramdomCoordinate(x2)
 	y2 = ramdomCoordinate(y2)
 	touchDown(1, x1, y1)
-    ramdomSleep(100)
+	ramdomSleep(100)
 	touchMove(1, x2, y2)
-    ramdomSleep(100)
+	ramdomSleep(100)
 	touchUp(1, x2, y2)
 end
